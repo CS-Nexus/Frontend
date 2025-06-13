47b83b86037bdfe4424a5f4958b2dbd9da7fefba
EHAB3x <ehabmah6969@gmail.com>
2025-06-13 06:53:18 +0200

    feat: add footer component with navigation links and social icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './layouts/header/header.component';
 import { initFlowbite } from 'flowbite';
+import { FooterComponent } from "./layouts/footer/footer.component";
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { faBrandLinkedinIn, faBrandInstagram } from '@ng-icons/font-awesome/brands';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  viewProviders: [provideIcons({
+    faBrandLinkedinIn,
+    faBrandInstagram
+  })]
 })
 export class FooterComponent {
 
