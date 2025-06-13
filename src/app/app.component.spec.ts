b696327e5f5390be49b14d6451cf8792c299eb6c
EHAB3x <ehabmah6969@gmail.com>
2025-06-13 06:02:27 +0200

    refactor: rename project from 'project-structure' to 'nexus' in configuration and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'project-structure' title`, () => {
+  it(`should have the 'nexus' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project-structure');
+    expect(app.title).toEqual('nexus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, project-structure');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nexus');
   });
 });
