a9b5e5dc7183feae23143fd0291093d9c8a40ea4
EHAB3x <ehabmah6969@gmail.com>
2025-06-14 09:01:11 +0200

    feat: implement ProjectOverviewComponent and integrate into home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeImageComponent } from "./components/home-image/home-image.component";
+import { ProjectOverviewComponent } from "./components/project-overview/project-overview.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeImageComponent],
+  imports: [HomeImageComponent, ProjectOverviewComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
