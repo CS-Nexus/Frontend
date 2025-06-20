65cc3a45041c86faa31b9970b9e29afc5ef995c8
EHAB3x <ehabmah6969@gmail.com>
2025-06-20 17:09:36 +0200

    feat: add environments and solutions constants with respective interfaces

---
+++ src/app/core/constants/solutions.ts
@@ -0,0 +1,20 @@
+import { ISolution } from "../interfaces/isolution";
+
+export const solutions: ISolution[] = [
+  {
+    title: 'Realistic VR Environments',
+    icon: 'bootstrapGlobe2',
+  },
+  {
+    title: 'Interactive Emergency Scenarios',
+    icon: 'bootstrapPuzzle',
+  },
+  {
+    title: 'Real-time Feedback and Assessment',
+    icon: 'bootstrapRepeat',
+  },
+  {
+    title: 'Multi-Platform Accessibility',
+    icon: 'bootstrapController',
+  },
+];
---
+++ src/app/core/interfaces/isolution.ts
@@ -0,0 +1,4 @@
+export interface ISolution {
+  title: string;
+  icon: string;
+}
