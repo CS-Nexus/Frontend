3ee37752b2950e39e1f8f2aa6884310217a71e72
EHAB3x <ehabmah6969@gmail.com>
2025-06-20 17:55:18 +0200

    feat: add milestones and IMilestone interface for project tracking

---
+++ src/app/core/constants/milestones.ts
@@ -0,0 +1,19 @@
+import { IMilestone } from '../interfaces/imilestone';
+
+export const mileStones: IMilestone[] = [
+  {
+    title: 'Environment and Scenario Development',
+    status: 'Completed',
+    icon: 'bootstrapCheck2',
+  },
+  {
+    title: 'User Testing and Refinement',
+    status: 'In Progress',
+    icon: 'bootstrapClock',
+  },
+  {
+    title: 'Platform Launch and Expansion',
+    status: 'Upcoming',
+    icon: 'bootstrapFlag',
+  },
+];
---
+++ src/app/core/interfaces/imilestone.ts
@@ -0,0 +1,5 @@
+export interface IMilestone {
+  title: string;
+  status: string;
+  icon: string;
+}
