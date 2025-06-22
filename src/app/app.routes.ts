7e92eb55363c2227a1630f4caa49b8fdb7e5c980
EHAB3x <ehabmah6969@gmail.com>
2025-06-22 20:21:39 +0200

    feat: add TeamPage component with routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProjectPageComponent } from './pages/project-page/project-page.component';
+import { TeamPageComponent } from './pages/team-page/team-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: HomePageComponent,
   },
   {
     path: 'project',
-    component: ProjectPageComponent
-  }
+    component: ProjectPageComponent,
+  },
+  {
+    path: 'team',
+    component: TeamPageComponent,
+  },
 ];
