4fe6c15ecd697de35d2aafa87ec82dc843f7e500
EHAB3x <ehabmah6969@gmail.com>
2025-07-04 17:22:26 +0200

    finish team page avatars content

--- src/app/pages/team-page/team-page.component.ts
+++ src/app/pages/team-page/team-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ITeamData } from '../../core/interfaces/iteam-data';
+import { teamData } from '../../core/constants/team';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-team-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './team-page.component.html',
   styleUrl: './team-page.component.scss'
 })
 export class TeamPageComponent {
+  teamDataArr : ITeamData[] = teamData;
+  activeImg: number = 0;
 
+  setActiveImg(index: number){
+    this.activeImg = index;
+  }
 }
